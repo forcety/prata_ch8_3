ad276620f304e04d985024c15c5163d25bf12316
forcety <forcety@mail.ru>
2018-05-05 13:53:16 +0300

    initial commit

---
+++ main.cpp
@@ -0,0 +1,41 @@
+#include <QCoreApplication>
+#include <iostream>
+
+
+using namespace std;
+
+void getUpperCaseString(string& str)
+{
+    char ch;
+
+    if (str.length() != 1 && str[0] != 'q')
+    {
+        for (int i = 0; i < str.length(); ++i) {
+            ch = str[i];
+            str[i] = (char)toupper(ch);
+        }
+    }
+
+
+//    ch = str[0];
+//    cout << ch << endl;
+//    cout << (char)toupper(ch) << endl;
+//    str[0] = (char)toupper(ch);
+
+}
+
+int main(int argc, char *argv[])
+{
+    std::cout << "Start program.\n";
+    QCoreApplication a(argc, argv);
+
+    cout << "Enter a string (q to quit):" << endl;
+    string input;
+    getline(cin, input);
+    cout << "Your initial string is: " << input << endl;
+
+    getUpperCaseString(input);
+    cout << "Result string is: " << input << endl;
+    cout << "Bye ";
+    return a.exec();
+}
